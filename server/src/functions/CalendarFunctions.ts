811d1a917524bcd5a1fb8f1b406116cdd5220817
Buymistr <dimkabumik@gmail.com>
2024-01-22 23:14:21 +0300

    Добавил методы для календаря

--- server/src/functions/CalendarFunctions.ts
+++ server/src/functions/CalendarFunctions.ts
@@ -1,13 +1,43 @@
 import Redis from "ioredis";
+import {CalendarItem} from "../models/CalendarItem";
+import {Task} from "../models/Task";
 
 export const getCalendar = async (redis: Redis) => {
-    return await redis.hget('calendar', 'field1')
+    const data = await redis.hgetall('calendar')
+
+    if (data) {
+        return Object.values(data).map((json) => JSON.parse(json))
+    }
+
+    return []
 }
 
-export const setCalendar = async (redis: Redis) => {
-    redis.hset('calendar', {
-        field1: 'field1',
-        field2: 'field2',
-        field3: 'field3',
-    })
+export const setCalendarItem = async (redis: Redis) => {
+    const id = new Date().getTime()
+    const date = new Date()
+
+    const calendarItem: CalendarItem = {
+        id,
+        date,
+        rowsCount: 0,
+        time: ''
+    }
+
+    const taskItem: Task = {
+        id,
+        date,
+        rows: [],
+        totalTime: ''
+    }
+
+    await redis.hset('calendar', id, JSON.stringify(calendarItem))
+    await redis.hset(`tasks`, id, JSON.stringify(taskItem))
+
+    return calendarItem
+}
+
+export const deleteCalendarItem = async (redis: Redis, { id }: { id: number }) => {
+    await redis.hdel('calendar', id.toString())
+    await redis.hdel('tasks', id.toString())
+    return id
 }
